0c9369a6bee7b7bb39a826951400f4963ffc6200
yanmingshuang <2314318218@qq.com>
2020-11-10 18:45:45 +0800

    master

--- webpackDemo/src/index.js
+++ webpackDemo/src/index.js
@@ -2,6 +2,7 @@ import createULElement from './eleComponents/list.js'
 import createP from './eleComponents/p.js'
 import createImg from './eleComponents/img'
 import './images/1.png'
+import "./css/index.scss"
 let root = document.querySelector("body")
 root.appendChild(createImg("./dist/images/1.png"))
 createP(root, '你好世界')
--- webpackDemo/webpack.config.js
+++ webpackDemo/webpack.config.js
@@ -1,3 +1,4 @@
+const autoprefixer = require('autoprefixer');
 let path = require('path');
 module.exports = {
   mode: "development", //开发模式（文件出现错误的话，指向具体文件）
@@ -10,14 +11,39 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.(jpg|png|gif|svg)$/, //文件名字后缀为这些的时候，就使用下面的file-loader
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: "[name].[ext]", //自己配置生产的图片的文件名，默认为hash 值
-          outputPath: "images" //图片文件打包到dist 下的images 文件夹下
+        test: /\.(jpg|png|gif|svg)$/, //文件名字后缀为这些的时候，就使用下面的file-loader
+        // use: {
+        //   loader: 'file-loader', //作用是读取信息，进行一些操作
+        //   options: {
+        //     name: "[name].[ext]", //自己配置生产的图片的文件名，默认为hash 值
+        //     outputPath: "images" //图片文件打包到dist 下的images 文件夹下
+        //   }
+        // }
+        use: {
+          loader: 'url-loader', //作用是读取信息，进行一些操作
+          options: {
+            name: "[name].[ext]", //自己配置生产的图片的文件名，默认为hash 值
+            outputPath: "images", //图片文件打包到dist 下的images 文件夹下
+            limit: 2048 //小于这个大小的，就把文件转换成base64,单位是字节
+          }
         }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          //从下往上调用loader
+          "style-loader", //一般放到上面
+          // "css-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2 //表示：引入css-loader 的时候必须引入下面两个loader,如果有3个，就写3
+            }
+          },
+          "sass-loader",
+          "postcss-loader", //给一些浏览器不兼容的样式加-webkit前缀,需要在package.json 添加"browserslist": ["> 1%"]，表示使用率在1%以上的浏览器
+        ]
       }
-    }]
+    ]
   }
 }
