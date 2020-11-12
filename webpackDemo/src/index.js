598c5dc850cd3c6056e83c224d5e2e45bc4d37cc
yanmingshuang <2314318218@qq.com>
2020-11-12 14:18:42 +0800

    'plugin插件安装'

--- webpackDemo/src/index.js
+++ webpackDemo/src/index.js
@@ -4,6 +4,6 @@ import createImg from './eleComponents/img'
 import './images/1.png'
 import "./css/index.scss"
 let root = document.querySelector("body")
-root.appendChild(createImg("./dist/images/1.png"))
+root.appendChild(createImg("./images/1.png"))
 createP(root, '你好世界')
 createULElement(root, 4)
--- webpackDemo/webpack.config.js
+++ webpackDemo/webpack.config.js
@@ -1,32 +1,36 @@
 const autoprefixer = require('autoprefixer');
 let path = require('path');
+let htmlWebpackPlugin = require('html-webpack-plugin');
+let {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 module.exports = {
   mode: "development", //开发模式（文件出现错误的话，指向具体文件）
   // mode: "production", //生产模式（文件出现错误的话，指向打包文件）
   entry: './src/index.js', //入口文件
   output: {
-    filename: "wanzhang.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
     publicPath: './'
   },
   module: {
     rules: [{
         test: /\.(jpg|png|gif|svg)$/, //文件名字后缀为这些的时候，就使用下面的file-loader
-        // use: {
-        //   loader: 'file-loader', //作用是读取信息，进行一些操作
-        //   options: {
-        //     name: "[name].[ext]", //自己配置生产的图片的文件名，默认为hash 值
-        //     outputPath: "images" //图片文件打包到dist 下的images 文件夹下
-        //   }
-        // }
         use: {
-          loader: 'url-loader', //作用是读取信息，进行一些操作
+          loader: 'file-loader', //作用是读取信息，进行一些操作
           options: {
             name: "[name].[ext]", //自己配置生产的图片的文件名，默认为hash 值
-            outputPath: "images", //图片文件打包到dist 下的images 文件夹下
-            limit: 2048 //小于这个大小的，就把文件转换成base64,单位是字节
+            outputPath: "images" //图片文件打包到dist 下的images 文件夹下
           }
         }
+        // use: {
+        //   loader: 'url-loader', //作用是读取信息，进行一些操作
+        //   options: {
+        //     name: "[name].[ext]", //自己配置生产的图片的文件名，默认为hash 值
+        //     outputPath: "images", //图片文件打包到dist 下的images 文件夹下
+        //     limit: 2048 //小于这个大小的，就把文件转换成base64,单位是字节
+        //   }
+        // }
       },
       {
         test: /\.(css|scss)$/,
@@ -43,7 +47,25 @@ module.exports = {
           "sass-loader",
           "postcss-loader", //给一些浏览器不兼容的样式加-webkit前缀,需要在package.json 添加"browserslist": ["> 1%"]，表示使用率在1%以上的浏览器
         ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/, //iconfont 图标引入
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: "./font",
+            name: "[name].[ext]"
+          }
+        }
       }
     ]
-  }
+  },
+  plugins: [
+    //需要下载插件，作用是在dist 文件夹下生成index.html 文件
+    new htmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    //需要下载插件，作用是每次打包之后删除在dist 文件夹之前的打包文件
+    new CleanWebpackPlugin()
+  ]
 }
